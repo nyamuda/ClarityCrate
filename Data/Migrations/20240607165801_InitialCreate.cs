effe4b5256fa9841ef1abb42bba5f8ce35529aee
P.T Nyamuda <ptnrlab@gmail.com>
2024-06-07 19:31:17 +0200

    MudBlazor drawer issue fixed

---
+++ Data/Migrations/20240607165801_InitialCreate.cs
@@ -0,0 +1,259 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clarity_Crate.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Curriculum",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curriculum", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Level",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Level", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subject",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Term",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Term", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Topic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topic", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CurriculumSubject",
+                columns: table => new
+                {
+                    CurriculumsId = table.Column<int>(type: "int", nullable: false),
+                    SubjectsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurriculumSubject", x => new { x.CurriculumsId, x.SubjectsId });
+                    table.ForeignKey(
+                        name: "FK_CurriculumSubject_Curriculum_CurriculumsId",
+                        column: x => x.CurriculumsId,
+                        principalTable: "Curriculum",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CurriculumSubject_Subject_SubjectsId",
+                        column: x => x.SubjectsId,
+                        principalTable: "Subject",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TermLevel",
+                columns: table => new
+                {
+                    LevelsId = table.Column<int>(type: "int", nullable: false),
+                    TermsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TermLevel", x => new { x.LevelsId, x.TermsId });
+                    table.ForeignKey(
+                        name: "FK_TermLevel_Level_LevelsId",
+                        column: x => x.LevelsId,
+                        principalTable: "Level",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TermLevel_Term_TermsId",
+                        column: x => x.TermsId,
+                        principalTable: "Term",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Definition",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TopicId = table.Column<int>(type: "int", nullable: false),
+                    CurriculumId = table.Column<int>(type: "int", nullable: false),
+                    SubjectId = table.Column<int>(type: "int", nullable: false),
+                    TermId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Definition", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Definition_Curriculum_CurriculumId",
+                        column: x => x.CurriculumId,
+                        principalTable: "Curriculum",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Definition_Subject_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subject",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Definition_Term_TermId",
+                        column: x => x.TermId,
+                        principalTable: "Term",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Definition_Topic_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TopicSubject",
+                columns: table => new
+                {
+                    SubjectsId = table.Column<int>(type: "int", nullable: false),
+                    TopicsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TopicSubject", x => new { x.SubjectsId, x.TopicsId });
+                    table.ForeignKey(
+                        name: "FK_TopicSubject_Subject_SubjectsId",
+                        column: x => x.SubjectsId,
+                        principalTable: "Subject",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TopicSubject_Topic_TopicsId",
+                        column: x => x.TopicsId,
+                        principalTable: "Topic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurriculumSubject_SubjectsId",
+                table: "CurriculumSubject",
+                column: "SubjectsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Definition_CurriculumId",
+                table: "Definition",
+                column: "CurriculumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Definition_SubjectId",
+                table: "Definition",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Definition_TermId",
+                table: "Definition",
+                column: "TermId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Definition_TopicId",
+                table: "Definition",
+                column: "TopicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TermLevel_TermsId",
+                table: "TermLevel",
+                column: "TermsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TopicSubject_TopicsId",
+                table: "TopicSubject",
+                column: "TopicsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurriculumSubject");
+
+            migrationBuilder.DropTable(
+                name: "Definition");
+
+            migrationBuilder.DropTable(
+                name: "TermLevel");
+
+            migrationBuilder.DropTable(
+                name: "TopicSubject");
+
+            migrationBuilder.DropTable(
+                name: "Curriculum");
+
+            migrationBuilder.DropTable(
+                name: "Level");
+
+            migrationBuilder.DropTable(
+                name: "Term");
+
+            migrationBuilder.DropTable(
+                name: "Subject");
+
+            migrationBuilder.DropTable(
+                name: "Topic");
+        }
+    }
+}
