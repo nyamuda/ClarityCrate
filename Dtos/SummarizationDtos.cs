6dde8907ecf36b6f60e19131a1e3c40d6dbd31ec
P.T Nyamuda <ptnrlab@gmail.com>
2024-11-18 02:21:45 +0200

    Paragraph summarization post request added

---
+++ Dtos/SummarizationDtos.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Clarity_Crate.Dtos
+{
+    public class SummaryDto
+{
+        public string summary { get; set; }
+}
+
+    public class SummaryRequestDto
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("max_length")]
+        public int Max_Length { get; set; }
+
+        [JsonPropertyName("min_length")]
+        public int Min_Length { get; set; }
+    }
+}
--- Services/SummarizationService.cs
+++ Services/SummarizationService.cs
@@ -1,11 +1,62 @@
-﻿namespace Clarity_Crate.Services
+﻿using Clarity_Crate.Dtos;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace Clarity_Crate.Services
+
 {
     public class SummarizationService
-{
+    {
 
         public string Text { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+
+        public bool IsSummarizing { get; set; }=false;
+        private HttpClient _httpClient;
+
+
+        public SummarizationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
 
 
-}
+        public async Task GetParagraphSummary(string text, int maxLength, int minLength)
+        {
+
+            string Url = "https://clarity-crate-fine-tuned-model.onrender.com/summarize";
+            var requestBody = new SummaryRequestDto
+            {
+                Text = text,
+                Max_Length = maxLength,
+                Min_Length = minLength
+            };
+
+            IsSummarizing = true;
+
+            var response = await _httpClient.PostAsJsonAsync(Url, requestBody);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var body = await response.Content.ReadFromJsonAsync<SummaryDto>();
+                Summary = body.summary;
+                IsSummarizing = false;
+            }
+
+
+          else
+            {
+                IsSummarizing = false;
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Error: {response.StatusCode}, Details: {errorContent}");
+                
+            }
+
+
+
+        }
+
+
+    }
 }
